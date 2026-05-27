19ca54d72bb283937e82b52297b40a3d5d9bb677
BayrolM <Iteasistemaagenciadeviajes@gmail.com>
2026-05-26 22:55:28 -0500

    fix: change output directory to dist for Vercel deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -50,10 +50,7 @@
         '@': path.resolve(__dirname, './src'),
       },
     },
-    build: {
-      target: 'esnext',
-      outDir: 'build',
-    },
+
     server: {
       port: 5173,
       open: true,
